c45c4a93959664bdc6f7f816aff55637d516ed74
Mathews Vinoy <mathewsvinoy@gmail.com>
2025-09-30 12:05:37 +0530

    cretaed basic datastruct

--- main.c
+++ main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
+#include <arpa/inet.h> 
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h> 
+#include <sys/socket.h>
+static int globid =0;
 
-struct file{:}
+struct DataNode{
+    int id;
+    char ip_addr[100];
+    int port;
+};
+struct Block{
+    int bId;
+    DtaNode* address[100];
+};
+stuct fileMetadata{
+    char filename[100];
+    Block* subids[100];
+};
 
-int main(){
+int main(){ 
     printf("Checkmate");
     return 0;
+}
+
+int globidGenerater(){
+    return globid++;
 }
