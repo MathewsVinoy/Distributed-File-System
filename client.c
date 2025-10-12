f00501776cfb32e1c2970f652ca500e83dc13f52
Mathews Vinoy <mathewsvinoy@gmail.com>
2025-10-12 18:38:05 +0530

    recve data from dataserver

--- client.c
+++ client.c
@@ -5,6 +5,12 @@
 #include <string.h>
 #include <sys/socket.h>
 
+typedef struct {
+    int blockid;
+    char location[50];
+    int port;
+} block;
+
 int main(){
     int clint_socket = socket(AF_INET, SOCK_STREAM,0);
     if(clint_socket==-1){
@@ -23,20 +29,37 @@ int main(){
     }
     char comment[100] = "LOOKUP my_file.txt BLOCK 0";
     send(clint_socket,comment,sizeof(comment),0);
-    char location[1024];
-    memset(location, 0, sizeof(location));
-    recv(clint_socket, location, sizeof(location), 0);
-    printf("The location of the data server is: %s\n", location);
+    block location;
+    recv(clint_socket, &location, sizeof(location), 0);
+    printf("The location of the data server is: %d\n", location.port);
 
     close(clint_socket);
+    
 
     // connection to the dataserver for getting data
-    int clint_socket = socket(AF_INET, SOCK_STREAM,0);
+    clint_socket = socket(AF_INET, SOCK_STREAM,0);
     if(clint_socket==-1){
         perror("Socket cretaion failed");
         exit(EXIT_FAILURE);
     } 
-    sever_addr.sin_port =htons(8000);
+    server_addr.sin_port =htons(location.port);
+    server_addr.sin_addr.s_addr = inet_addr(location.location);
+
+    if(connect(clint_socket,(struct sockaddr *)&server_addr, sizeof(server_addr))==-1){
+        perror("Socket cretaion failed");
+        close(clint_socket);
+        exit(EXIT_FAILURE);
+    }
+
+    free(comment);
+
+    comment = "GET_BLOCK 0";
+    send(clint_socket,comment,sizeof(comment),0);
+    char *buffer;
+    recv(clint_socket,buffer, sizeof(buffer),0);
+    printf("%s",buffer);
+
+
 
  
     return 0;
