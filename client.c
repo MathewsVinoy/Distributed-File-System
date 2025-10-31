3d1902a0b49b75015b1667b54d7560c9f5a24b7b
Mathews Vinoy <mathewsvinoy@gmail.com>
2025-11-01 00:09:59 +0530

    if one server is down or do not have the data then go to the next in the list sent by the meta data server

--- client.c
+++ client.c
@@ -7,8 +7,8 @@
 
 typedef struct {
     int blockid;
-    char location[50];
-    int port;
+    char locations[50][50];
+    int ports[50];
 } block;
 
 int main(){
@@ -34,40 +34,47 @@ int main(){
 
     block location;
     recv(clint_socket, &location, sizeof(location), 0);
-    printf("The location of the data server is: %s:%d\n", location.location, location.port);
+    
 
     close(clint_socket);
 
     // Connect to the data server
-    clint_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if(clint_socket == -1){
-        perror("Socket creation failed");
-        exit(EXIT_FAILURE);
+    for(int i=0;i<sizeof(location.locations);i++){
+
+        clint_socket = socket(AF_INET, SOCK_STREAM, 0);
+        if(clint_socket == -1){
+            perror("Socket creation failed");
+            continue;
+        }
+
+        server_addr.sin_port = htons(location.ports[i]);
+        server_addr.sin_addr.s_addr = inet_addr(location.locations[i]);
+
+        if(connect(clint_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1){
+            perror("Data server connection failed");
+            close(clint_socket);
+            continue;
+        }
+
+        // Correct way to send GET request
+        char request[50] = "GET_BLOCK 0";
+        send(clint_socket, request, strlen(request), 0);
+
+        char buffer[1024];  // allocate proper space
+        memset(buffer, 0, sizeof(buffer));
+        ssize_t n =recv(clint_socket, buffer, sizeof(buffer), 0);
+
+        if (n > 0) {
+            buffer[n] = '\0';
+            printf("Received data: %s\n", buffer);
+            close(clint_socket);
+            break;
+        }else {
+            perror("Failed to receive data");
+            close(clint_socket);
+            continue; // Try next DS
+        }
     }
-
-    server_addr.sin_port = htons(location.port);
-    server_addr.sin_addr.s_addr = inet_addr(location.location);
-
-    if(connect(clint_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1){
-        perror("Data server connection failed");
-        close(clint_socket);
-        exit(EXIT_FAILURE);
-    }
-
-    // Correct way to send GET request
-    char request[50] = "GET_BLOCK 0";
-    send(clint_socket, request, strlen(request), 0);
-
-    char buffer[1024];  // allocate proper space
-    memset(buffer, 0, sizeof(buffer));
-    ssize_t n =recv(clint_socket, buffer, sizeof(buffer), 0);
-
-    if (n > 0) {
-        buffer[n] = '\0';
-        printf("Received data: %s\n", buffer);
-    }
-
-    close(clint_socket);
     return 0;
 }
 
