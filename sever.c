cd37bc7996f6b01f5e338d93823db7cfb78626a7
Mathews Vinoy <mathewsvinoy@gmail.com>
2025-11-01 23:29:29 +0530

    going to add the feach PUT to the server

--- sever.c
+++ sever.c
@@ -5,16 +5,33 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-int main(){
+int main(int argc, char *argv[]){
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    int port = atoi(argv[1]);
+    if (port <= 0) {
+        fprintf(stderr, "Invalid port number\n");
+        exit(EXIT_FAILURE);
+    }
     int server_fd = socket(AF_INET,SOCK_STREAM,0);
     if(server_fd <0){
         perror("Socket connection failed");
         exit(EXIT_FAILURE);
     }
+
+    int opt = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+        perror("setsockopt failed");
+        exit(EXIT_FAILURE);
+    }
+
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(8000);
+    server_addr.sin_port = htons(port);
 
     if(bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0){
         perror("Blint Failed");
@@ -46,7 +63,7 @@ int main(){
         size_t r = fread(buffer, 1, sizeof(buffer), fp1);
         send(clint_sock, buffer, r, 0);
         printf("send successfully\n");
-    }
+    }else if (strcmp(request, "PUT BLOCK 1"))
     close(clint_sock);
     close(server_fd);
     fclose(fp1);
