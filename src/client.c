fcca5152cea86458de26dd709d971a5d54912cf5
Mathews Vinoy <mathewsvinoy@gmail.com>
2025-11-22 20:12:44 +0530

    made some changes

--- src/client.c
+++ src/client.c
@@ -1,80 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <string.h>
-#include <sys/socket.h>
 #include "datastruct.h"
 #include "clint/connction_cash.h"
-
+#include "clint/lookup.h"
+#include "clint/writefuncs.h"
+#include "clint/read_file.h"
 
 int main(){
-    int meta_socket = socket(AF_INET, SOCK_STREAM, 0);
-    char comment[100] ;
-    int n;
-    printf("\t\tTHIS IS AN CLINT CONNECTION.");
-    printf("\nWhich fuction shoult you need to perform?");
-    printf("1.For Reading block\n2.for writing on to the block");
-    scanf("%d",&n);
-    switch(n){
-        case 1:break;
-        case 2: break;
-        case 3: break;
-        case 4: comment = = "GET_FILE_MAP my_file.txt";
-        break;
+    int option;
+    printf("\n\t\t\tEnter the operatrion that need to be performed: ");
+    printf("\n1 for displaying block\n2 for write to the file\n 3 for  showing the full file");
+    scanf("%d",&option);
+    switch(option){
+        case 1: lookupBlock(); break;
+        case 2: writeFile(); break;
+        case 3: readFile(); break;
+        default: return 0;
     }
-    if(meta_socket == -1){
-        perror("Socket creation failed");
-        exit(EXIT_FAILURE);
-    }
-     struct sockaddr_in server_addr;
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(9000);
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    if(connect(meta_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1){
-        perror("Metadata server connection failed");
-        close(meta_socket);
-        exit(EXIT_FAILURE);
-    }
-
-
-    FILE *fp1 = fopen("out/cli/myfile.txt", "a");
-    
-    send(meta_socket, comment, strlen(comment), 0);
-
-    FileMap datamap;  
-    recv(meta_socket, &datamap, sizeof(datamap), 0);
-    printf("Received file map for %s with %d blocks\n", datamap.filename, datamap.total_blocks);
-
-    for(int i =0; i<datamap.total_blocks;i++){
-        for(size_t j=0;j<sizeof(datamap.blocks[i].locations);j++){
-            int clint_socket = get_connection(datamap.blocks[i].locations[j],datamap.blocks[i].ports[j]);
-            char request[100];
-            sprintf(request , "%s %d","GET BLOCK",datamap.blocks[i].blockid);
-            send(clint_socket, request, strlen(request), 0);
-
-            char buffer[1024];  
-            memset(buffer, 0, sizeof(buffer));
-            ssize_t n =recv(clint_socket, buffer, sizeof(buffer), 0);
-
-            if (n > 0) {
-                buffer[n] = '\0';
-                printf("Received data: %s\n", buffer);
-                fprintf(fp1, "%s", buffer);
-                release_connection(clint_socket);
-                break;
-            }else {
-                perror("Failed to receive data");
-                release_connection(clint_socket);
-                continue;
-            }
-        } 
-    }
-
-    close(meta_socket);
-    close_all_connections();
-    fclose(fp1);
     return 0;
-
 }
+
