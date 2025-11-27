f1cf7e6df94562b7c72ba58f7c3acc81ac2e7061
Mathews Vinoy <mathewsvinoy@gmail.com>
2025-11-27 15:52:57 +0530

    update

---
+++ src/metadata_res/getlocation.c
@@ -0,0 +1,24 @@
+#include "datastruct.h"
+
+
+blockinfo get_location(char* filename, int block_id){
+    FileMap file_map = findlocation(filename);
+
+    blockinfo b;
+    memset(&b, 0, sizeof(b));
+    b.blockid = -1;
+
+    /* ensure the returned FileMap matches the requested filename */
+    if (strcmp(file_map.filename, filename) != 0) {
+        return b;
+    }
+
+    for (int j = 0; j < file_map.total_blocks; j++) {
+        if (file_map.blocks[j].blockid == block_id) {
+            return file_map.blocks[j];
+        }
+    }
+
+    return b; /* not found */
+}
+
