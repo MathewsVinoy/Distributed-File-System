96c2b60f73df98595f24fef1fc60c8906288318b
Mathews Vinoy <mathewsvinoy@gmail.com>
2025-12-04 22:55:27 +0530

    created the update

--- src/metadata_server.c
+++ src/metadata_server.c
@@ -6,28 +6,8 @@
 #include <sys/socket.h>
 #include "load_metadata.h"
 #include "datastruct.h"
+#include "metadata/getlocation.h"
 
-
-blockinfo get_location(char* filename, int block_id){
-    FileMap file_map = findlocation(filename);
-
-    blockinfo b;
-    memset(&b, 0, sizeof(b));
-    b.blockid = -1;
-
-    /* ensure the returned FileMap matches the requested filename */
-    if (strcmp(file_map.filename, filename) != 0) {
-        return b;
-    }
-
-    for (int j = 0; j < file_map.total_blocks; j++) {
-        if (file_map.blocks[j].blockid == block_id) {
-            return file_map.blocks[j];
-        }
-    }
-
-    return b; /* not found */
-}
 
 void split(const char* input, char* filename, int* block_id, char* command){
     /* Robust tokenization: command filename [blockid] */
