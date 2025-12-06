13cc50f1d9f0a8db1eba994c2791f9bc5f180530
Mathews Vinoy <mathewsvinoy@gmail.com>
2025-12-06 23:53:17 +0530

    ...

--- src/metadata_res/writeblock.c
+++ src/metadata_res/writeblock.c
@@ -1,3 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include "load_metadata.h"
+#include "datastruct.h"
+
+void writeblock(){
+    
+}
 
