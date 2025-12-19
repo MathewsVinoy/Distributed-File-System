35e45eb110523e1a9ed5c0705047505ced2936d4
Mathews Vinoy <mathewsvinoy@gmail.com>
2025-12-20 01:21:17 +0530

    add an new fun

--- src/metadata_res/writeblock.c
+++ src/metadata_res/writeblock.c
@@ -7,14 +7,17 @@
 #include "load_metadata.h"
 #include "datastruct.h"
 #include "metadata/getlocation.h"
+#include "clint/connction_cash.h"
 
-
-void writeblock (int server_fd){
+void writeblock (int server_fd, char filename[100]){
     FileMap full_map = findlocation(filename);
     int i;
     blockinfo block;
     for(i=0;i<full_map.total_blocks;i++){
-        block = fullmap.blocks[i];
+        int clint_socket = get_connection(datamap.blocks[i].locations[j],datamap.blocks[i].ports[j]);
+        char request[100];
+        
+        
         //complete the connection code later 
         //need to connect to all other servers and send an requsite that need to send data or  not
     }
