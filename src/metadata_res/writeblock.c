cc29107cea0217ddf95a39284abb5f2ae8b119f5
Mathews Vinoy <mathewsvinoy@gmail.com>
2025-12-25 01:13:25 +0530

    going to implement the file logic for recive the global msgs

--- src/metadata_res/writeblock.c
+++ src/metadata_res/writeblock.c
@@ -8,42 +8,26 @@
 #include "datastruct.h"
 #include "clint/connction_cash.h"
 
-int send_(int server_fd, FileMap full_map,int no,char msg[100]){
-    FileMapchar buffer[100], msg[50];
-    char buffer[100];
-    int i,j;
-    for(i=0;i<full_map.total_blocks;i++){
-        if(full_map.blocks[i].blockid==no){
-            for(j=0;j<sizeof(full_map.blocks[i].locations),j++){
-                int clint_socket = get_connection(full_map.blocks[i].locations[j],full_map.blocks[i].ports[j]);                     send(clint_socket,msg,sizeof(msg),0);
-                ssize_t recv_bytes = recv(clint_socket, buffer, sizeof(buffer) - 1, 0);
-                if (recv_bytes <= 0) {
-                    close(clint_socket);
-                    return 1;
-                }
-                if(strcmp(buffer, "ABORT")==0){
-                    close(clint_socket);
-                    return 1;
-                }
+int* write_block(FileMap datamap, int blockid){
+    char msg;
+    int n=0, socket_addrs[100];
+    sprintf(msg, "PREPARE_WRITE BLOCK %d",blockid);
+    for(int i =0; i<datamap.total_blocks;i++){
+        if(datamap.blocks[i].blockid==blockid){
+            for(size_t j=0;j<sizeof(datamap.blocks[i].locations);j++){
+                int clint_socket = get_connection(datamap.blocks[i].locations[j],datamap.blocks[i].ports[j]);
+                send(clint_socket, msg, strlen(msg), 0);
+                socket_addrs[n]=clint_socket;
+                n++;
             }
         }
     }
-    return 0;
+    //send completed 
+    return socket_addrs
 }
 
-void write_block(int server_fd, FileMap full_map,int no){
-    char msg[100];
-    int n;
-    sprintf(msg,"PREPARE_WRITE BLOCK %d",no);
-    int about=send_msg(server_fd,full_map,no,msg);
-    if(about==1){
-        sprintf(msg,"GLOBAL_ABORT BLOCK %d",no);
-        n=send_mg(server_fd,full_map,no,msg);
-    }else{
-        sprintf(msg,"GLOBAL_COMMIT BLOCK %d",no);
-        n=send_mg(server_fd,full_map,no,msg);
+void status_listen(int *ds_lists){
+    for(int i=0; i<sizeof(ds_list)){
+
     }
 }
-
-
-
