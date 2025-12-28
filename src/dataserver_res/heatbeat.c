8126177328ee81bdd44ead9929ffe793443d0def
Mathews Vinoy <mathewsvinoy@gmail.com>
2025-12-28 12:15:13 +0530

    sending input corrected

--- src/dataserver_res/heatbeat.c
+++ src/dataserver_res/heatbeat.c
@@ -1,11 +1,12 @@
 #include "dataserver/heatbeat.h"
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 void *heardbeat(void *arg){
-    (void)arg;
+    int port = *((int*)arg);
     while(1){
-        printf("\n\n\t\t\tHeart Beat sented\n\n");
+        printf("\n\n\t\t\tHeart Beat sented = %d\n\n",port);
         sleep(5);
     }
     return NULL;
