b9030e06a246404f4b94bc892d07cfbc8c8eb03d
MathewsVinoy <mathewsvinoy@gmail.com>
2025-12-31 13:34:30 +0000

    ..

--- include/clint/connction_cash.h
+++ include/clint/connction_cash.h
@@ -1,7 +1,7 @@
 #ifndef CONNCTION_CASH_H
 #define CONNCTION_CASH_H
 
-int get_connection(const char ip[50], int port);
+int get_connection(const char *ip, int port);
 int release_connection(int socket_fd);
 void close_all_connections();
 
