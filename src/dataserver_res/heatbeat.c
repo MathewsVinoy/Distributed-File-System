4ee9268d5659aeb0cc7baf568f306ddc1b36dc2d
MathewsVinoy <mathewsvinoy@gmail.com>
2026-01-05 15:06:22 +0000

    made the headbeat basic set up

--- src/dataserver_res/heatbeat.c
+++ src/dataserver_res/heatbeat.c
@@ -2,12 +2,21 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include "clint/connction_cash.h"
 
 void *heardbeat(void *arg){
     int port = *((int*)arg);
+    int metaserver = get_connection("127.0.0.1",port);
+    char msg[100];
+    sprintf(msg,"HEARTBEAT DS_ID:%d",port);
     while(1){
-        printf("\n\n\t\t\tHeart Beat sented = %d\n\n",port);
+        send(metaserver,msg,strlen(msg),0);
+        printf("\nsend the msg\n");
         sleep(5);
     }
+    close(metaserver);
     return NULL;
 }
