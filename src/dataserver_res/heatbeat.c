cf38472db458f00f02db598b5231d90322242089
Mathews_Vinoy <mathewsvinoy@gmail.com>
2026-06-07 15:04:54 +0000

    ..

--- src/dataserver_res/heatbeat.c
+++ src/dataserver_res/heatbeat.c
@@ -1,22 +1,63 @@
 #include "dataserver/heatbeat.h"
+#include "common/logging.h"
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include "clint/connction_cash.h"
+#include <errno.h>
+#include <arpa/inet.h>
 
-void *heardbeat(void *arg){
-    int port = *((int*)arg);
-    int metaserver = get_connection("127.0.0.1",port);
-    char msg[100];
-    sprintf(msg,"HEARTBEAT DS_ID:%d",port);
-    while(1){
-        send(metaserver,msg,strlen(msg),0);
-        printf("\nsend the msg\n");
+static int heartbeat_connect(const dataserver_heartbeat_args_t *cfg) {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        LOG_ERROR("Heartbeat: socket() failed - %s", strerror(errno));
+        return -1;
+    }
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(cfg->port);
+    if (inet_pton(AF_INET, cfg->host, &addr.sin_addr) <= 0) {
+        LOG_ERROR("Heartbeat: invalid host %s", cfg->host);
+        close(sock);
+        return -1;
+    }
+    if (connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        close(sock);
+        return -1;
+    }
+    return sock;
+}
+
+void *heartbeat(void *arg){
+    if (!arg) {
+        LOG_ERROR("Heartbeat: Invalid argument");
+        pthread_exit(NULL);
+    }
+
+    dataserver_heartbeat_args_t cfg;
+    memcpy(&cfg, arg, sizeof(cfg));
+    free(arg);
+
+    char msg[128];
+    snprintf(msg, sizeof(msg), "HEARTBEAT DS_ID %d", cfg.data_server_port);
+
+    while (1) {
+        int sock = heartbeat_connect(&cfg);
+        if (sock >= 0) {
+            if (send(sock, msg, strlen(msg), 0) < 0) {
+                LOG_WARN("Heartbeat send failed: %s", strerror(errno));
+            } else {
+                LOG_DEBUG("Heartbeat sent for DS %d", cfg.data_server_port);
+            }
+            close(sock);
+        } else {
+            LOG_WARN("Heartbeat: unable to reach metadata server %s:%d", cfg.host, cfg.port);
+        }
         sleep(5);
     }
-    close(metaserver);
+
     return NULL;
 }
